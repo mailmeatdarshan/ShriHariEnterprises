44c50ccbd5907337bcba1a389a500e1f530a7fcb
mailmeatdarshan <mailmeatdarshan@gmail.com>
2026-01-04 20:25:22 +0530

    User manangement-Create, Auth, Update

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 
 // Utiles
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -20,10 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// Routes
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { createUser, getAllUsers, getCurrentUserProfile, loginUser, logoutCurrentUser, updateCurrentUserProfile } from "../controllers/userController.js";
+import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router
+    .route("/")
+    .post(createUser)
+    .get(authenticate, authorizedAdmin, getAllUsers);
+
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser)
+
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile )
+
+export default router;
