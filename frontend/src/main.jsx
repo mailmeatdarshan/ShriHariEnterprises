e76fbd4faef66f44fdfd4922e23f71dd2a35f1bd
mailmeatdarshan <mailmeatdarshan@gmail.com>
2026-01-09 19:24:54 +0530

    completed update profile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,13 +7,25 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/features/store.js'
 
+//PrivateRoute
+import PrivateRoute from './components/PrivateRoute.jsx'  
+
+
 //Auth
 import Login from './pages/Auth/Login.jsx'
 import Register from './pages/Auth/Register.jsx'
 
+//User
+import Profile from './pages/User/Profile.jsx'
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}> 
+
+      <Route path="/" element={<PrivateRoute />}>
+      <Route path='/profile' element={<Profile />} />
+      </Route>
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Route>
