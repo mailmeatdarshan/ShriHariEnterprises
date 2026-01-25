2a125fa318fc4e6656f6cc29efff9966e27016c7
mailmeatdarshan <mailmeatdarshan@gmail.com>
2026-01-25 22:50:33 +0530

    Catogory Management Done

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,17 +1,17 @@
 import Category from "../models/categoryModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 
-const createCategory = asyncHandler(async(req,res)=>{
+const createCategory = asyncHandler(async (req, res) => {
     try {
-        const {name} = req.body
-        if(!name){
-            return res.status(400).json({Error:"Please provide a name"})
+        const { name } = req.body
+        if (!name) {
+            return res.status(400).json({ Error: "Please provide a name" })
         }
-        const existingCategory = await Category.findOne({name})
-        if(existingCategory){
-            return res.status(400).json({Error:"Category already exists"})
+        const existingCategory = await Category.findOne({ name })
+        if (existingCategory) {
+            return res.status(400).json({ Error: "Category already exists" })
         }
-        const category = await new Category({name}).save()
+        const category = await new Category({ name }).save()
         res.status(201).json(category)
     } catch (error) {
         console.log(error)
@@ -19,14 +19,14 @@ const createCategory = asyncHandler(async(req,res)=>{
     }
 })
 
-const updateCategory = asyncHandler(async(req,res)=>{
+const updateCategory = asyncHandler(async (req, res) => {
     try {
-        const {name} = req.body
-        const {categoryId} = req.params
+        const { name } = req.body
+        const { categoryId } = req.params
 
-        const category = await Category.findOne({ _id: categoryId})
-        if(!category){
-            return res.status(404).json({Error:"Category not found"})
+        const category = await Category.findOne({ _id: categoryId })
+        if (!category) {
+            return res.status(404).json({ Error: "Category not found" })
         }
         category.name = name
         const updatedCategory = await category.save()
@@ -34,41 +34,41 @@ const updateCategory = asyncHandler(async(req,res)=>{
 
     } catch (error) {
         console.log(error)
-        return res.status(500).json({Error:"Internal Server Error"})
+        return res.status(500).json({ Error: "Internal Server Error" })
     }
 })
 
-const removeCategory = asyncHandler(async(req,res)=>{
+const removeCategory = asyncHandler(async (req, res) => {
     try {
-        const removedCategory = await Category.findByIdAndRemove(req.params.categoryId)   
+        const removedCategory = await Category.findByIdAndDelete(req.params.categoryId)
         res.status(200).json(removedCategory)
     } catch (error) {
         console.log(error)
-        return res.status(500).json({Error:"Internal Server Error"})
+        return res.status(500).json({ Error: "Internal Server Error" })
     }
 })
 
-const listCategory = asyncHandler(async(req,res)=>{
+const listCategory = asyncHandler(async (req, res) => {
     try {
         const all = await Category.find({})
         res.json(all)
     } catch (error) {
         console.log(error)
-        return res.status(400).json({Error:"Internal Server Error"})
+        return res.status(400).json({ Error: "Internal Server Error" })
     }
 })
 
-const readCategory = asyncHandler(async(req,res)=>{
+const readCategory = asyncHandler(async (req, res) => {
     try {
-        const category = await Category.findOne({ _id: req.params.id})
+        const category = await Category.findOne({ _id: req.params.id })
         res.json(category)
     } catch (error) {
         console.log(error)
-        return res.status(400).json({Error:"Internal Server Error"})
+        return res.status(400).json({ Error: "Internal Server Error" })
     }
 })
 
-export { createCategory,updateCategory,removeCategory, listCategory,readCategory };
+export { createCategory, updateCategory, removeCategory, listCategory, readCategory };
 
 
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,6 +21,7 @@ import Profile from './pages/User/Profile.jsx'
 //Admin
 import AdminRoute from './pages/Admin/AdminRoute.jsx'
 import UserList from './pages/Admin/UserList.jsx'
+import CategoryList from './pages/Admin/CategoryList.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -34,7 +35,7 @@ const router = createBrowserRouter(
     {/* Admin Routes */}
     <Route path="/admin" element={<AdminRoute />}>
       <Route path='userlist' element={<UserList />} />
-
+      <Route path='categorylist' element={<CategoryList />} />
     </Route>
     </Route>
   )
