52f6b0fb2e1bf8662d939bd1f689aef53f24ca42
mailmeatdarshan <mailmeatdarshan@gmail.com>
2026-06-05 21:38:43 +0530

    Products management backend completed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,22 @@ import formidable from "express-formidable";
 
 const router = express.Router();
 
-import { addProducts, updateProductDetails } from "../controllers/productController.js";
+import { addProducts, updateProductDetails, removeProduct, fetchProducts, fetchProductById,fetchAllProducts, addProductReview, fetchTopProducts, fetchNewProducts } from "../controllers/productController.js";
 import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 
-router.route("/").post(authenticate, authorizedAdmin, formidable(), addProducts);
+router.route("/").get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProducts); 
 
-router.route("/:id").put(authenticate, authorizedAdmin, formidable(), updateProductDetails);
+router.route('/allproducts').get(fetchAllProducts);
+router.route('/:id/reviews').post(authenticate, authorizedAdmin, checkId, addProductReview)
+
+router.get('/top', fetchTopProducts )
+router.get('/new', fetchNewProducts )
+
+
+router.route("/:id")
+.get(fetchProductById)
+.put(authenticate, authorizedAdmin, formidable(), updateProductDetails)
+.delete(authenticate, authorizedAdmin, removeProduct)
 
 export default router;
